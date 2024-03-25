2107dd28574d35cded0e988388958fc8271d2140
Daly <dalyna64@gmail.com>
2024-03-26 00:46:43 +0100

    ui up 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              const style = document.createElement('style')
+              style.innerHTML = '@layer tailwind-base, primereact, tailwind-utilities;'
+              style.setAttribute('type', 'text/css')
+              document.querySelector('head').prepend(style)
+            `,
+          }}
+        />
+      </head>
+
       <body className={inter.className}>
         <Provider>{children}</Provider>
       </body>
