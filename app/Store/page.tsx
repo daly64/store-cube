e2c43713f34bb19afef354e803dd200e18dd26e1
Daly <41022166+daly64@users.noreply.github.com>
2024-04-06 17:12:20 +0100

    navigation system

--- app/Store/page.tsx
+++ app/Store/page.tsx
@@ -1,9 +1,27 @@
+'use client'
+import Dialogs from '@/components/Dialogs';
+import ProductsTable from '@/components/ProductsTable';
+import { toggleAddProductDialog } from '@/tools/client/zustand/productsStatesModifiers';
+import { Button } from 'primereact/button';
 import React from 'react'
 
 const StorePage = () => {
   return (
     <div>
       <h1>Store</h1>
+
+      <Dialogs />
+      <div className=" flex flex-row justify-end my-5 mx-5">
+        <Button
+          label="Add new product"
+          icon="pi pi-plus"
+          onClick={toggleAddProductDialog}
+        />
+      </div>
+
+      <div className=" m-auto w-3/4">
+        <ProductsTable />
+      </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 "use client";
-import Dialogs from "@/components/Dialogs";
-import ProductsTable from "@/components/ProductsTable";
 import useStore from "@/tools/client/zustand/producctStore";
 import { useRouter } from "next/navigation";
-import { Button } from "primereact/button";
 import { useEffect } from "react";
 export default function Home() {
   const { toggleAddProductDialog } = useStore();
@@ -13,18 +10,7 @@ useEffect(()=>{
 })
   return (
     <main>
-      <Dialogs />
-      <div className=" flex flex-row justify-end my-5 mx-5">
-        <Button
-          label="Add new product"
-          icon="pi pi-plus"
-          onClick={toggleAddProductDialog}
-        />
-      </div>
-
-      <div className=" m-auto w-3/4">
-        <ProductsTable />
-      </div>
+     
       
     </main>
   );
