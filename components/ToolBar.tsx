76b5edc13c006d41fc88d0f43b332e2bd5e4a042
Daly <41022166+daly64@users.noreply.github.com>
2024-04-06 11:13:56 +0100

    ui fix

--- components/ToolBar.tsx
+++ components/ToolBar.tsx
@@ -1,18 +1,35 @@
+import { Menubar } from "primereact/menubar";
 import { Toolbar } from "primereact/toolbar";
-import React from "react";
 ;
 
 const ToolBar = () => {
   return (
-    <Toolbar
-      className="h-16  flex justify-between items-center bg-white text-gray-600 border-b-2 border-gray-300"
+    // <Toolbar
+    //   className=" flex justify-between items-center bg-white border-b border-gray-300"
+    //   style={{ gap: "1rem" }}
+    //   start={
+    //     <div className="flex items-center">
+    //       <img src="/logo.svg" className="h-8 mr-2" alt="logo" />
+    //       <h2 className="text-xl font-semibold">Store Cube</h2>
+
+    //     </div>
+    //   }
+    // />
+
+    <Menubar
       start={
         <div className="flex items-center">
-          
-          <img src='/logo.svg' className="h-8 mr-4" alt="logo" />
+          <img src="/logo.svg" className="h-8 mr-2" alt="logo" />
           <h2 className="text-xl font-semibold">Store Cube</h2>
         </div>
       }
+      style={{ gap: "1rem" }}
+      model={[
+        { label: "Dashboard", icon: "pi pi-fw pi-desktop" },
+        { label: "List", icon: "pi pi-fw pi-list" },
+        { label: "Inputs", icon: "pi pi-fw pi-sign-in" },
+        { label: "Outputs", icon: "pi pi-fw pi-sign-out" },
+      ]}
     />
   );
 };
